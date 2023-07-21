108047de56861e5a7d71b1bce696b77fa8665f17
Moises <investmentmoises@gmail.com>
2023-07-21 02:28:12 -0300

    adicionando entidades

---
+++ src/entities/ProductEntiti.ts
@@ -0,0 +1,7 @@
+export interface ProductEntiti {
+    _id: string,
+    name: string,
+    description: string,
+    price: number,
+    image: string
+}
---
+++ src/entities/UserEntiti.ts
@@ -0,0 +1,6 @@
+export interface UserEntiti {
+    _id: string,
+    name: string,
+    email: string,
+    password: string
+}
