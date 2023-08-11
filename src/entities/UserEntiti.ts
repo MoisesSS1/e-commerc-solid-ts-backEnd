c0495a144db460043fab0ccedffd1d7d76455597
Moises <investmentmoises@gmail.com>
2023-08-11 01:44:45 -0300

    Validação de criação da classe usuário

--- src/entities/UserEntiti.ts
+++ src/entities/UserEntiti.ts
@@ -11,6 +11,10 @@ export class UserEntiti {
 
 
     constructor({ name, email, password }: User) {
+
+        if (!name || !email || !password || name.length < 3 || email.length < 6 || password.length < 6) {
+            throw new Error("Preencha os dados corretamente!")
+        }
         this.email = email
         this.name = name
         this.password = password
