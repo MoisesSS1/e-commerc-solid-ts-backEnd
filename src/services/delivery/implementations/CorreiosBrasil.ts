c3f88321b7769624ff3cedbea06c24a4f78b41d3
MoisesSS1 <investmentmoises@gmail.com>
2023-09-05 07:17:02 -0300

    update erro api entregas

--- src/services/delivery/implementations/CorreiosBrasil.ts
+++ src/services/delivery/implementations/CorreiosBrasil.ts
@@ -5,7 +5,11 @@ import { consultarCep } from "correios-brasil/dist";
 export class CorreiosBrasil implements IDelivery {
 
     async getAdress(cep: string): Promise<IgetAdress> {
-        const checkAdress = await consultarCep(cep)
+        const checkAdress: any = await consultarCep(cep)
+
+        if (checkAdress.erro === true) {
+            throw new Error("Cep não encontrado!")
+        }
         return checkAdress
     }
 
